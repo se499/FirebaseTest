7ecf020f4754bf8053cd8b222852ba6b8484654e
S NJ <sengnja2017@gmail.com>
2021-04-15 21:51:50 +0630

    First commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+var firebaseConfig = 
+{
+    apiKey: "AIzaSyDejmidl7GbFGDpoaoz2OQB24eH1nIYyE8",
+    authDomain: "fir-webapp-f9445.firebaseapp.com",
+    projectId: "fir-webapp-f9445",
+    storageBucket: "fir-webapp-f9445.appspot.com",
+    messagingSenderId: "862360001220",
+    appId: "1:862360001220:web:e30fe0d92879cdbca5c791",
+    measurementId: "G-0XTXKB0TD6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.auth.Auth.Persistence.LOCAL;
+  firebase.auth.Auth.Persistence.SESSION;
+  
+  $("#btn-login").click(function(){
+      var email = $("#email").val();
+      var password = $("#password").val();
+
+      if(email != "" && password != null)
+      {
+        var result = firebase.auth().signInWithEmailAndPassword(email.value, password.value);
+
+        result.catch(function(error)
+        {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMessage);
+            window.alert("Message : " + errorMessage);
+        });
+      }
+      else
+      {
+        window.alert("Please fill out all fields.");
+      }
+
+  });
